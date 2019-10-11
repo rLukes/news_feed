7f2320e1852801a98063e3e8005c7c87281d4b6d
Ruben Lukes <lukesruben@gmail.com>
2019-10-11 09:24:15 +0200

    added sqflite package

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
